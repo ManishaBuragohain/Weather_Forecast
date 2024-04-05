0c6a0349f6f24b0487338d454b7f0ccddc6e4a14
Manisha <flowerdark777@gmail.com>
2024-04-05 23:36:38 +0530

    About text modified

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -30,17 +30,7 @@ function About() {
               meteorology, SkyView Weather aims to deliver accurate, accessible,
               and aesthetically pleasing weather forecasts to users worldwide.
               Our Mission is simple: to demystify the weather, making forecast
-              data easy to understand and accessible for everyone. Whether
-              you're planning your week, deciding on the best day for an outdoor
-              event, or just curious about the weather patterns, SkyView Weather
-              is here to serve. Features at a Glance: Precise Forecasts:
-              Leveraging cutting-edge weather APIs, SkyView delivers accurate
-              current conditions and forecasts. User-Friendly Design: Our
-              application is designed with you in mind - intuitive, simple, and
-              effective. Interactive Elements: Toggle between Celsius and
-              Fahrenheit, view hourly details, and more - all at your
-              fingertips. Globally Local: From the largest metropolises to the
-              smallest towns, get the forecast for your location.
+              data easy to understand and accessible for everyone.
             </p>
           </div>
         </div>
